ffa6bfb7987de687301c5bbffe0ffdece1456a39
kaur-Gurdeep <gagakaur188@gmail.com>
2025-04-11 19:19:04 -0700

    Fix: correct CSS module path

--- src/components/WatchLater/WatchLater.jsx
+++ src/components/WatchLater/WatchLater.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "./watchLater.module.css";
+import styles from "./WatchLater.module.css";
 
 function WatchLater({ watchLater, setWatchLater }) {
   // Handle remove from watch later
