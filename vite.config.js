6deae49b03d28f82c7431984f1750e450ab1b2ae
kaur-Gurdeep <gagakaur188@gmail.com>
2025-04-14 17:35:55 -0700

    Redeploy

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: '/movie-app-dev/',
   css: {
     devSourcemap: true,
   },
-
-  base: '/movie-app-dev/',
 
   server: {
     host: '0.0.0.0',
